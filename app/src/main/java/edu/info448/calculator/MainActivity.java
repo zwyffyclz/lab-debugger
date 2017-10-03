546109035d7b4fb9f8dd2f00bb262c5372242eeb
Wuyi Zhang <wuyi_zhang@outlook.com>
2017-10-02 22:43:13 -0700

    Fixed errors in the Android Calculator application

--- app/src/main/java/edu/info448/calculator/MainActivity.java
+++ app/src/main/java/edu/info448/calculator/MainActivity.java
@@ -54,10 +54,11 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 try {
-                    compute(Calculator.Operator.MUL);
+                    compute(Calculator.Operator.DIV);
                 } catch (IllegalArgumentException iae) {
                     Log.e(TAG, "IllegalArgumentException", iae);
                     mResultTextView.setText(getString(R.string.computationError));
+                    mOperandEditText.setText(null);
                 }
             }
         });
@@ -75,6 +76,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 mResultTextView.setText(R.string.initial_result_text);
+                mCalculator.setValue(0.0);
             }
         });
     }
@@ -96,7 +98,7 @@ public class MainActivity extends Activity {
                 result = String.valueOf(mCalculator.add(operand));
                 break;
             case SUB:
-                result = getString(R.string.computationError);
+                result = String.valueOf(mCalculator.sub(operand));
                 break;
             case DIV:
                 result = String.valueOf(mCalculator.div(operand));
